680f6b9c203bf0a87a802c4e448d69cd253e3636
SzPetra <petra.szilagyi27@gmail.com>
2023-02-06 16:14:02 +0100

    add: my goals page

---
+++ src/myGoalsPage/MyGoalsPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MyGoalsPage = () => {
+    return (
+        <p>This is my goals page</p>
+    )
+}
+
+export default MyGoalsPage;
--- src/uiKit/components/Header.js
+++ src/uiKit/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Header = () => {
 
     return (
         <>
-            <nav>this is the header</nav>
+            <nav className="nav-container">
+                
+            </nav>
             <Outlet />
         </>
     )
